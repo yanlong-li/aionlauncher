f786c67dade0eea1e703e3ca2b594d4d93dd3066
unknown <Yanlongli@tcmcares.com>
2017-10-27 17:15:55 +0800

    添加项目文件。

---
+++ aionroylauncher/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace aionroylauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serverAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.port = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(373, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 132);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(61, 209);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "账号登陆";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(114, 240);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(111, 21);
+            this.username.TabIndex = 2;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(114, 277);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(111, 21);
+            this.password.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "账号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "密码";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "服务器地址：";
+            // 
+            // serverAddress
+            // 
+            this.serverAddress.Location = new System.Drawing.Point(166, 310);
+            this.serverAddress.Name = "serverAddress";
+            this.serverAddress.Size = new System.Drawing.Size(177, 21);
+            this.serverAddress.TabIndex = 7;
+            this.serverAddress.Text = "127.0.0.1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(368, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "端口号：";
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(427, 310);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(78, 21);
+            this.port.TabIndex = 9;
+            this.port.Text = "2106";
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(592, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 38);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(127)))), ((int)(((byte)(205)))));
+            this.ClientSize = new System.Drawing.Size(643, 344);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.serverAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AION启动器";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox serverAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ aionroylauncher/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace aionroylauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //禁用账号密码输入
+            this.username.ReadOnly = true;
+            this.password.ReadOnly = true;
+            //Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            //string username = config.AppSettings.Settings["username"].Value;
+            //string password = config.AppSettings.Settings["password"].Value;
+            //string serverAddress = config.AppSettings.Settings["serverAddress"].Value;
+            //string autoLogin = config.AppSettings.Settings["autoLogin"].Value;
+            string username = Properties.Settings.Default.username;
+            string password = Properties.Settings.Default.password;
+            string serverAddress = Properties.Settings.Default.serverAddress;
+            bool autoLogin = Properties.Settings.Default.autoLogin;
+            if (username.Length > 0) this.username.Text = username;
+            if (password.Length > 0) this.password.Text = password;
+            if (serverAddress.Length > 0) this.serverAddress.Text = serverAddress;
+            if (autoLogin) { this.checkBox1.Checked = true; } else { this.checkBox1.Checked = false; }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            string serverAddress = "sdjk.f3322.net";
+            string port = "2106";
+            string username = "";
+            string password = "";
+            string userlogin = "";
+            if (this.checkBox1.Checked)
+            {
+                username = this.username.Text;
+                password = this.password.Text;
+                if (username.Length < 1 || password.Length < 1) { MessageBox.Show("请输入账号或密码"); return; }
+                userlogin = " -account:" + username + " -password:" + password + " ";
+                Properties.Settings.Default.username = username;
+                Properties.Settings.Default.password = password;
+            }
+            if (this.port.Text.Length >= 1)
+            {
+                port = this.port.Text;
+            }
+            if (this.serverAddress.Text.Length >= 3)
+            {
+                if (ValidateIPAddress(this.serverAddress.Text))//是不是IP地址
+                {
+                    serverAddress = this.serverAddress.Text;//是直接写入
+                }
+                else
+                {
+                    //否，获取url的IP地址
+                    IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(this.serverAddress.Text);
+                    System.Net.IPAddress[] addr = ipEntry.AddressList;
+                    serverAddress = addr[0].ToString();
+                }
+
+                
+            }
+            string sd = "/" + System.Environment.SystemDirectory.Substring(0, 1) + " ";
+            var startInfo = new ProcessStartInfo
+            {
+
+                FileName = "cmd.exe",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                Arguments = sd+" start bin32\\aion.bin -ip:"+serverAddress+" -port:"+port+" -cc:5 -noauthgg -noweb -nb -ingameshop -nowebshop "+ userlogin
+                //Arguments = sd + "start bin32\\aion.bin -ip:192.168.1.5 -port:2106 -cc:5 -noauthgg -noweb -nb -ingameshop -nowebshop"
+            };
+            Process.Start(startInfo);
+            MessageBox.Show("正在启动");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!this.checkBox1.Checked)
+            {
+                this.username.ReadOnly = true;
+                this.password.ReadOnly = true;
+                Properties.Settings.Default.autoLogin = false;
+            }
+            else
+            {
+                this.username.ReadOnly = false;
+                this.password.ReadOnly = false;
+                Properties.Settings.Default.autoLogin = true;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        public static bool ValidateIPAddress(string ipAddress)
+        {
+            Regex validipregex = new Regex(@"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+            return (ipAddress != "" && validipregex.IsMatch(ipAddress.Trim())) ? true : false;
+        }
+    }
+}
